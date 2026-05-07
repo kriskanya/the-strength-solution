48353524cc6816c374eceebeffec6b65c0a8fd4d
Kristian Kanya <kristiankanya@gmail.com>
2026-05-06 21:41:33 -0700

    change image on right-side of landing page to be a dashboard screenshot

--- app/components/home/LandingPageRight.tsx
+++ app/components/home/LandingPageRight.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
+
+import dashboardScreenshot from '@/app/images/dashboard-screenshot.png'
+
 import classes from './LandingPageRight.module.css'
 
 export default function LandingPageRight() {
   return (
-    <div className={`${classes.container} w-full h-full`}></div>
+    <div className={`${classes.container} relative min-h-screen w-full`}>
+      <Image
+        src={dashboardScreenshot}
+        alt="The Strength Solution dashboard with strength assessments, muscle map, and progress charts."
+        className={`${classes.image} object-cover object-left-top`}
+        fill
+        priority
+        sizes="50vw"
+      />
+    </div>
   )
 }
